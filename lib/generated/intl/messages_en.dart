4b37196a73763f9dbc7211f256ff7e9e68bf57c8
rang <378694192@qq.com>
2025-10-25 17:53:26 +0800

    去掉版本+

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -28,207 +28,199 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "account": MessageLookupByLibrary.simpleMessage("Account"),
-        "accountSwitch": MessageLookupByLibrary.simpleMessage("Switch Account"),
-        "appSettings": MessageLookupByLibrary.simpleMessage("App Settings"),
-        "appearance": MessageLookupByLibrary.simpleMessage("Appearance"),
-        "autoTurnOnSleepTimer": MessageLookupByLibrary.simpleMessage(
-          "Auto Turn On Sleep Timer",
-        ),
-        "automaticallyDescription": MessageLookupByLibrary.simpleMessage(
-          "Automatically turn on the sleep timer based on the time of day",
-        ),
-        "backup": MessageLookupByLibrary.simpleMessage("Backup"),
-        "backupAndRestore": MessageLookupByLibrary.simpleMessage(
-          "Backup and Restore",
-        ),
-        "bookAbout": MessageLookupByLibrary.simpleMessage("About the Book"),
-        "bookAboutDefault": MessageLookupByLibrary.simpleMessage(
-          "Sorry, no description found",
-        ),
-        "bookAuthors": MessageLookupByLibrary.simpleMessage("Authors"),
-        "bookDownloads": MessageLookupByLibrary.simpleMessage("Downloads"),
-        "bookGenres": MessageLookupByLibrary.simpleMessage("Genres"),
-        "bookMetadataAbridged":
-            MessageLookupByLibrary.simpleMessage("Abridged"),
-        "bookMetadataLength": MessageLookupByLibrary.simpleMessage("Length"),
-        "bookMetadataPublished":
-            MessageLookupByLibrary.simpleMessage("Published"),
-        "bookMetadataUnabridged": MessageLookupByLibrary.simpleMessage(
-          "Unabridged",
-        ),
-        "bookSeries": MessageLookupByLibrary.simpleMessage("Series"),
-        "bookShelveEmpty": MessageLookupByLibrary.simpleMessage("Try again"),
-        "bookShelveEmptyText": MessageLookupByLibrary.simpleMessage(
-          "No shelves to display",
-        ),
-        "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
-        "copyToClipboard": MessageLookupByLibrary.simpleMessage(
-          "Copy to Clipboard",
-        ),
-        "copyToClipboardDescription": MessageLookupByLibrary.simpleMessage(
-          "Copy the app settings to the clipboard",
-        ),
-        "copyToClipboardToast": MessageLookupByLibrary.simpleMessage(
-          "Settings copied to clipboard",
-        ),
-        "delete": MessageLookupByLibrary.simpleMessage("Delete"),
-        "deleteDialog": m0,
-        "deleted": m1,
-        "explore": MessageLookupByLibrary.simpleMessage("explore"),
-        "exploreHint": MessageLookupByLibrary.simpleMessage(
-          "Seek and you shall discover...",
-        ),
-        "exploreTooltip": MessageLookupByLibrary.simpleMessage(
-          "Search and Explore",
-        ),
-        "general": MessageLookupByLibrary.simpleMessage("General"),
-        "help": MessageLookupByLibrary.simpleMessage("Help"),
-        "home": MessageLookupByLibrary.simpleMessage("Home"),
-        "homeBookContinueListening": MessageLookupByLibrary.simpleMessage(
-          "Continue Listening",
-        ),
-        "homeBookContinueSeries": MessageLookupByLibrary.simpleMessage(
-          "Continue Series",
-        ),
-        "homeBookDiscover": MessageLookupByLibrary.simpleMessage("Discover"),
-        "homeBookListenAgain":
-            MessageLookupByLibrary.simpleMessage("Listen Again"),
-        "homeBookNewestAuthors": MessageLookupByLibrary.simpleMessage(
-          "Newest Authors",
-        ),
-        "homeBookRecentlyAdded": MessageLookupByLibrary.simpleMessage(
-          "Recently Added",
-        ),
-        "homeBookRecommended":
-            MessageLookupByLibrary.simpleMessage("Recommended"),
-        "homeContinueListening": MessageLookupByLibrary.simpleMessage(
-          "Continue Listening",
-        ),
-        "homeListenAgain": MessageLookupByLibrary.simpleMessage("Listen Again"),
-        "homePageSettings": MessageLookupByLibrary.simpleMessage(
-          "Home Page Settings",
-        ),
-        "homePageSettingsDescription": MessageLookupByLibrary.simpleMessage(
-          "Customize the home page",
-        ),
-        "homeStartListening": MessageLookupByLibrary.simpleMessage(
-          "Start Listening",
-        ),
-        "language": MessageLookupByLibrary.simpleMessage("Language"),
-        "languageDescription": MessageLookupByLibrary.simpleMessage(
-          "Language switch",
-        ),
-        "library": MessageLookupByLibrary.simpleMessage("Library"),
-        "libraryChange": MessageLookupByLibrary.simpleMessage("Change Library"),
-        "libraryEmpty": MessageLookupByLibrary.simpleMessage(
-          "No libraries available.",
-        ),
-        "libraryLoadError": m2,
-        "librarySelect": MessageLookupByLibrary.simpleMessage("Select Library"),
-        "librarySwitchTooltip": MessageLookupByLibrary.simpleMessage(
-          "Switch Library",
-        ),
-        "libraryTooltip": MessageLookupByLibrary.simpleMessage(
-          "Browse your library",
-        ),
-        "loading": MessageLookupByLibrary.simpleMessage("Loading..."),
-        "logs": MessageLookupByLibrary.simpleMessage("Logs"),
-        "no": MessageLookupByLibrary.simpleMessage("No"),
-        "notImplemented":
-            MessageLookupByLibrary.simpleMessage("Not implemented"),
-        "notificationMediaPlayer": MessageLookupByLibrary.simpleMessage(
-          "Notification Media Player",
-        ),
-        "notificationMediaPlayerDescription":
-            MessageLookupByLibrary.simpleMessage(
-          "Customize the media player in notifications",
-        ),
-        "ok": MessageLookupByLibrary.simpleMessage("OK"),
-        "pause": MessageLookupByLibrary.simpleMessage("Pause"),
-        "play": MessageLookupByLibrary.simpleMessage("Play"),
-        "playerSettings":
-            MessageLookupByLibrary.simpleMessage("Player Settings"),
-        "playerSettingsDescription": MessageLookupByLibrary.simpleMessage(
-          "Customize the player settings",
-        ),
-        "playerSettingsPlaybackReporting": MessageLookupByLibrary.simpleMessage(
-          "Playback Reporting",
-        ),
-        "playerSettingsPlaybackReportingIgnore":
-            MessageLookupByLibrary.simpleMessage(
+    "account": MessageLookupByLibrary.simpleMessage("Account"),
+    "accountSwitch": MessageLookupByLibrary.simpleMessage("Switch Account"),
+    "appSettings": MessageLookupByLibrary.simpleMessage("App Settings"),
+    "appearance": MessageLookupByLibrary.simpleMessage("Appearance"),
+    "autoTurnOnSleepTimer": MessageLookupByLibrary.simpleMessage(
+      "Auto Turn On Sleep Timer",
+    ),
+    "automaticallyDescription": MessageLookupByLibrary.simpleMessage(
+      "Automatically turn on the sleep timer based on the time of day",
+    ),
+    "backup": MessageLookupByLibrary.simpleMessage("Backup"),
+    "backupAndRestore": MessageLookupByLibrary.simpleMessage(
+      "Backup and Restore",
+    ),
+    "bookAbout": MessageLookupByLibrary.simpleMessage("About the Book"),
+    "bookAboutDefault": MessageLookupByLibrary.simpleMessage(
+      "Sorry, no description found",
+    ),
+    "bookAuthors": MessageLookupByLibrary.simpleMessage("Authors"),
+    "bookDownloads": MessageLookupByLibrary.simpleMessage("Downloads"),
+    "bookGenres": MessageLookupByLibrary.simpleMessage("Genres"),
+    "bookMetadataAbridged": MessageLookupByLibrary.simpleMessage("Abridged"),
+    "bookMetadataLength": MessageLookupByLibrary.simpleMessage("Length"),
+    "bookMetadataPublished": MessageLookupByLibrary.simpleMessage("Published"),
+    "bookMetadataUnabridged": MessageLookupByLibrary.simpleMessage(
+      "Unabridged",
+    ),
+    "bookSeries": MessageLookupByLibrary.simpleMessage("Series"),
+    "bookShelveEmpty": MessageLookupByLibrary.simpleMessage("Try again"),
+    "bookShelveEmptyText": MessageLookupByLibrary.simpleMessage(
+      "No shelves to display",
+    ),
+    "cancel": MessageLookupByLibrary.simpleMessage("Cancel"),
+    "copyToClipboard": MessageLookupByLibrary.simpleMessage(
+      "Copy to Clipboard",
+    ),
+    "copyToClipboardDescription": MessageLookupByLibrary.simpleMessage(
+      "Copy the app settings to the clipboard",
+    ),
+    "copyToClipboardToast": MessageLookupByLibrary.simpleMessage(
+      "Settings copied to clipboard",
+    ),
+    "delete": MessageLookupByLibrary.simpleMessage("Delete"),
+    "deleteDialog": m0,
+    "deleted": m1,
+    "explore": MessageLookupByLibrary.simpleMessage("explore"),
+    "exploreHint": MessageLookupByLibrary.simpleMessage(
+      "Seek and you shall discover...",
+    ),
+    "exploreTooltip": MessageLookupByLibrary.simpleMessage(
+      "Search and Explore",
+    ),
+    "general": MessageLookupByLibrary.simpleMessage("General"),
+    "help": MessageLookupByLibrary.simpleMessage("Help"),
+    "home": MessageLookupByLibrary.simpleMessage("Home"),
+    "homeBookContinueListening": MessageLookupByLibrary.simpleMessage(
+      "Continue Listening",
+    ),
+    "homeBookContinueSeries": MessageLookupByLibrary.simpleMessage(
+      "Continue Series",
+    ),
+    "homeBookDiscover": MessageLookupByLibrary.simpleMessage("Discover"),
+    "homeBookListenAgain": MessageLookupByLibrary.simpleMessage("Listen Again"),
+    "homeBookNewestAuthors": MessageLookupByLibrary.simpleMessage(
+      "Newest Authors",
+    ),
+    "homeBookRecentlyAdded": MessageLookupByLibrary.simpleMessage(
+      "Recently Added",
+    ),
+    "homeBookRecommended": MessageLookupByLibrary.simpleMessage("Recommended"),
+    "homeContinueListening": MessageLookupByLibrary.simpleMessage(
+      "Continue Listening",
+    ),
+    "homeListenAgain": MessageLookupByLibrary.simpleMessage("Listen Again"),
+    "homePageSettings": MessageLookupByLibrary.simpleMessage(
+      "Home Page Settings",
+    ),
+    "homePageSettingsDescription": MessageLookupByLibrary.simpleMessage(
+      "Customize the home page",
+    ),
+    "homeStartListening": MessageLookupByLibrary.simpleMessage(
+      "Start Listening",
+    ),
+    "language": MessageLookupByLibrary.simpleMessage("Language"),
+    "languageDescription": MessageLookupByLibrary.simpleMessage(
+      "Language switch",
+    ),
+    "library": MessageLookupByLibrary.simpleMessage("Library"),
+    "libraryChange": MessageLookupByLibrary.simpleMessage("Change Library"),
+    "libraryEmpty": MessageLookupByLibrary.simpleMessage(
+      "No libraries available.",
+    ),
+    "libraryLoadError": m2,
+    "librarySelect": MessageLookupByLibrary.simpleMessage("Select Library"),
+    "librarySwitchTooltip": MessageLookupByLibrary.simpleMessage(
+      "Switch Library",
+    ),
+    "libraryTooltip": MessageLookupByLibrary.simpleMessage(
+      "Browse your library",
+    ),
+    "loading": MessageLookupByLibrary.simpleMessage("Loading..."),
+    "logs": MessageLookupByLibrary.simpleMessage("Logs"),
+    "no": MessageLookupByLibrary.simpleMessage("No"),
+    "notImplemented": MessageLookupByLibrary.simpleMessage("Not implemented"),
+    "notificationMediaPlayer": MessageLookupByLibrary.simpleMessage(
+      "Notification Media Player",
+    ),
+    "notificationMediaPlayerDescription": MessageLookupByLibrary.simpleMessage(
+      "Customize the media player in notifications",
+    ),
+    "ok": MessageLookupByLibrary.simpleMessage("OK"),
+    "pause": MessageLookupByLibrary.simpleMessage("Pause"),
+    "play": MessageLookupByLibrary.simpleMessage("Play"),
+    "playerSettings": MessageLookupByLibrary.simpleMessage("Player Settings"),
+    "playerSettingsDescription": MessageLookupByLibrary.simpleMessage(
+      "Customize the player settings",
+    ),
+    "playerSettingsPlaybackReporting": MessageLookupByLibrary.simpleMessage(
+      "Playback Reporting",
+    ),
+    "playerSettingsPlaybackReportingIgnore":
+        MessageLookupByLibrary.simpleMessage(
           "Ignore Playback Position Less Than",
         ),
-        "playerSettingsPlaybackReportingMinimum":
-            MessageLookupByLibrary.simpleMessage("Minimum Position to Report"),
-        "playerSettingsPlaybackReportingMinimumDescriptionHead":
-            MessageLookupByLibrary.simpleMessage(
+    "playerSettingsPlaybackReportingMinimum":
+        MessageLookupByLibrary.simpleMessage("Minimum Position to Report"),
+    "playerSettingsPlaybackReportingMinimumDescriptionHead":
+        MessageLookupByLibrary.simpleMessage(
           "Do not report playback for the first ",
         ),
-        "playerSettingsPlaybackReportingMinimumDescriptionTail":
-            MessageLookupByLibrary.simpleMessage("of the book"),
-        "playerSettingsRememberForEveryBook":
-            MessageLookupByLibrary.simpleMessage(
-          "Remember Player Settings for Every Book",
-        ),
-        "playerSettingsRememberForEveryBookDescription":
-            MessageLookupByLibrary.simpleMessage(
+    "playerSettingsPlaybackReportingMinimumDescriptionTail":
+        MessageLookupByLibrary.simpleMessage("of the book"),
+    "playerSettingsRememberForEveryBook": MessageLookupByLibrary.simpleMessage(
+      "Remember Player Settings for Every Book",
+    ),
+    "playerSettingsRememberForEveryBookDescription":
+        MessageLookupByLibrary.simpleMessage(
           "Settings like speed, loudness, etc. will be remembered for every book",
         ),
-        "playerSettingsSpeedDefault": MessageLookupByLibrary.simpleMessage(
-          "Default Speed",
-        ),
-        "playerSettingsSpeedOptions": MessageLookupByLibrary.simpleMessage(
-          "Speed Options",
-        ),
-        "playlistsMine": MessageLookupByLibrary.simpleMessage("My Playlists"),
-        "readLess": MessageLookupByLibrary.simpleMessage("Read Less"),
-        "readMore": MessageLookupByLibrary.simpleMessage("Read More"),
-        "refresh": MessageLookupByLibrary.simpleMessage("Refresh"),
-        "reset": MessageLookupByLibrary.simpleMessage("Reset"),
-        "resetAppSettings": MessageLookupByLibrary.simpleMessage(
-          "Reset App Settings",
-        ),
-        "resetAppSettingsDescription": MessageLookupByLibrary.simpleMessage(
-          "Reset the app settings to the default values",
-        ),
-        "resetAppSettingsDialog": MessageLookupByLibrary.simpleMessage(
-          "Are you sure you want to reset the app settings?",
-        ),
-        "restore": MessageLookupByLibrary.simpleMessage("Restore"),
-        "restoreBackup": MessageLookupByLibrary.simpleMessage("Restore Backup"),
-        "restoreBackupHint": MessageLookupByLibrary.simpleMessage(
-          "Paste the backup here",
-        ),
-        "restoreBackupInvalid": MessageLookupByLibrary.simpleMessage(
-          "Invalid backup",
-        ),
-        "restoreBackupSuccess": MessageLookupByLibrary.simpleMessage(
-          "Settings restored",
-        ),
-        "restoreBackupValidator": MessageLookupByLibrary.simpleMessage(
-          "Please paste the backup here",
-        ),
-        "restoreDescription": MessageLookupByLibrary.simpleMessage(
-          "Restore the app settings from the backup",
-        ),
-        "resume": MessageLookupByLibrary.simpleMessage("Resume"),
-        "retry": MessageLookupByLibrary.simpleMessage("Retry"),
-        "settings": MessageLookupByLibrary.simpleMessage("Settings"),
-        "shakeDetector": MessageLookupByLibrary.simpleMessage("Shake Detector"),
-        "shakeDetectorDescription": MessageLookupByLibrary.simpleMessage(
-          "Customize the shake detector settings",
-        ),
-        "themeSettings": MessageLookupByLibrary.simpleMessage("Theme Settings"),
-        "themeSettingsDescription": MessageLookupByLibrary.simpleMessage(
-          "Customize the app theme",
-        ),
-        "unknown": MessageLookupByLibrary.simpleMessage("Unknown"),
-        "webVersion": MessageLookupByLibrary.simpleMessage("Web Version"),
-        "yes": MessageLookupByLibrary.simpleMessage("Yes"),
-        "you": MessageLookupByLibrary.simpleMessage("You"),
-        "youTooltip": MessageLookupByLibrary.simpleMessage(
-          "Your Profile and Settings",
-        ),
-      };
+    "playerSettingsSpeedDefault": MessageLookupByLibrary.simpleMessage(
+      "Default Speed",
+    ),
+    "playerSettingsSpeedOptions": MessageLookupByLibrary.simpleMessage(
+      "Speed Options",
+    ),
+    "playlistsMine": MessageLookupByLibrary.simpleMessage("My Playlists"),
+    "readLess": MessageLookupByLibrary.simpleMessage("Read Less"),
+    "readMore": MessageLookupByLibrary.simpleMessage("Read More"),
+    "refresh": MessageLookupByLibrary.simpleMessage("Refresh"),
+    "reset": MessageLookupByLibrary.simpleMessage("Reset"),
+    "resetAppSettings": MessageLookupByLibrary.simpleMessage(
+      "Reset App Settings",
+    ),
+    "resetAppSettingsDescription": MessageLookupByLibrary.simpleMessage(
+      "Reset the app settings to the default values",
+    ),
+    "resetAppSettingsDialog": MessageLookupByLibrary.simpleMessage(
+      "Are you sure you want to reset the app settings?",
+    ),
+    "restore": MessageLookupByLibrary.simpleMessage("Restore"),
+    "restoreBackup": MessageLookupByLibrary.simpleMessage("Restore Backup"),
+    "restoreBackupHint": MessageLookupByLibrary.simpleMessage(
+      "Paste the backup here",
+    ),
+    "restoreBackupInvalid": MessageLookupByLibrary.simpleMessage(
+      "Invalid backup",
+    ),
+    "restoreBackupSuccess": MessageLookupByLibrary.simpleMessage(
+      "Settings restored",
+    ),
+    "restoreBackupValidator": MessageLookupByLibrary.simpleMessage(
+      "Please paste the backup here",
+    ),
+    "restoreDescription": MessageLookupByLibrary.simpleMessage(
+      "Restore the app settings from the backup",
+    ),
+    "resume": MessageLookupByLibrary.simpleMessage("Resume"),
+    "retry": MessageLookupByLibrary.simpleMessage("Retry"),
+    "settings": MessageLookupByLibrary.simpleMessage("Settings"),
+    "shakeDetector": MessageLookupByLibrary.simpleMessage("Shake Detector"),
+    "shakeDetectorDescription": MessageLookupByLibrary.simpleMessage(
+      "Customize the shake detector settings",
+    ),
+    "themeSettings": MessageLookupByLibrary.simpleMessage("Theme Settings"),
+    "themeSettingsDescription": MessageLookupByLibrary.simpleMessage(
+      "Customize the app theme",
+    ),
+    "unknown": MessageLookupByLibrary.simpleMessage("Unknown"),
+    "webVersion": MessageLookupByLibrary.simpleMessage("Web Version"),
+    "yes": MessageLookupByLibrary.simpleMessage("Yes"),
+    "you": MessageLookupByLibrary.simpleMessage("You"),
+    "youTooltip": MessageLookupByLibrary.simpleMessage(
+      "Your Profile and Settings",
+    ),
+  };
 }
